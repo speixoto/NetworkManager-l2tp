16275a66aeb44bf76e5b1707fdb83674463f67ff
Sérgio Peixoto <matematico2002@hotmail.com>
2015-03-02 14:59:34 +0000

    Update nm-l2tp-service.c
    Remove @ from gateway id

--- src/nm-l2tp-service.c
+++ src/nm-l2tp-service.c
@@ -902,7 +902,7 @@ nm_l2tp_start_ipsec(NML2tpPlugin *plugin,
 	fprintf(fp, "%s%s ",value?"@":"", value?value:"%any");
 
 	value = nm_setting_vpn_get_data_item (s_vpn, NM_L2TP_KEY_IPSEC_GATEWAY_ID);
-	fprintf(fp, "%s%s ",value?"@":"", value?value:"%any");
+	fprintf(fp, "%s ", value?value:"%any");
 
 	value = nm_setting_vpn_get_data_item (s_vpn, NM_L2TP_KEY_IPSEC_PSK);
 	if(!value)value="";
